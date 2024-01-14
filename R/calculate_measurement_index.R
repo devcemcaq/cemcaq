b7486b9b97089f03882316b4a7ad7854356deedf
Brayan Hernández Cortés <brayannhc07@gmail.com>
2024-01-13 20:44:39 -0600

    feat: Testing calculate_measurements_index

--- R/calculate_measurement_index.R
+++ R/calculate_measurement_index.R
@@ -1,39 +1,74 @@
-calculate_measurements_index <- function(measurements, hours = 1, weighted = FALSE) {
+calculate_measurements_index <- function(measurements, hours = 1, weighted = FALSE, relevant_gap = 3, min_relevant_records = 2) {
   if (hours < 1) {
     stop("Hours must be greater than zero")
   }
 
   if (hours == 1) {
-    # Promedio horario
-    return(ifelse(length(measurements) > 1, measurements[1], measurements))
+    return(get_last_hour_measurement(measurements))
   }
 
   if (weighted) {
-    # Concentracion promedio movil ponderado
-    factor <- 0
-    divider <- 0
-    weight <- obtener_pesos(measurements)
-    for (i in 0:(hours - 1)) {
-      value <- measurements[hours - i]
-      if (is.na(value) || is.null(value)) {
-        next
-      }
-      factor <- factor + (value * weight^i)
-      divider <- divider + (weight^i)
-    }
-    media_movil_ponderada <- factor / divider
-    return(media_movil_ponderada)
+    return(calculate_weighted_moving_concentration(measurements, hours, relevant_gap, min_relevant_records))
   }
 
-  return(mean(measurements))
+  return(calculate_hourly_average(measurements, hours))
 }
 
-concentracion_movil_ponderada <- function(x) {
-  weight <- obtener_pesos(x)
-  return((sum(x) * weight) / sum(weight))
+get_last_hour_measurement <- function(measurements) {
+  return(tail(measurements, n = 1))
 }
 
-obtener_pesos <- function(x) {
+calculate_hourly_average <- function(measurements, hours, percentage_min_records = 0.75) {
+  min_records <- floor(hours * percentage_min_records)
+  clean_measurements <- measurements[!is.na(measurements)]
+  if (length(clean_measurements) < min_records) {
+    return(NA)
+  }
+  return(mean(clean_measurements))
+}
+
+calculate_weighted_moving_concentration <- function(measurements, hours, relevant_gap, min_relevant_records) {
+  if (length(measurements) != hours) {
+    stop("Measurements count must be equals to the number of hours to evaluate")
+  }
+
+  if (relevant_gap < 0) {
+    stop("Relevant gap must be greater than zero")
+  }
+
+  if (relevant_gap > length(measurements)) {
+    stop("Relevant gap must be less or equal than the number of hours to evaluate")
+  }
+
+  if (min_relevant_records <= 0) {
+    stop("Minimum number of relevant records must be greater than zero")
+  }
+
+  if (min_relevant_records > relevant_gap) {
+    stop("Minimum number of relevant records must be less or equal than the relevant gap value")
+  }
+
+  relevant_gap <- tail(measurements, n = relevant_gap)
+  if (length(relevant_gap[!is.na(relevant_gap)]) < min_relevant_records) {
+    return(NA)
+  }
+
+  weight <- get_moving_concentration_weight(measurements)
+  factor <- 0
+  divider <- 0
+  for (i in 0:(hours - 1)) {
+    value <- measurements[hours - i]
+    if (is.na(value) || is.null(value)) {
+      next
+    }
+    factor <- factor + (value * weight^i)
+    divider <- divider + (weight^i)
+  }
+  media_movil_ponderada <- factor / divider
+  return(media_movil_ponderada)
+}
+
+get_moving_concentration_weight <- function(x) {
   x <- x[!is.na(x)]
   weight <- 1 - (max(x) - min(x)) / max(x)
   return(ifelse(weight > 0.5, round(weight, 2), 0.5))
--- tests/testthat/test-calculate_measurement_index.R
+++ tests/testthat/test-calculate_measurement_index.R
@@ -1,68 +1,122 @@
-test_that("get average hourly concentration from scalar value", {
-  measurement <- runif(1, min = 1, max = 10)
+test_that("Obtener el indice horario de un valor escalar", {
+  measurements <- runif(1, min = 1, max = 10)
 
-  expect_equal(calculate_measurements_index(measurement, hours = 1), measurement)
+  expect_equal(calculate_measurements_index(measurements, hours = 1), measurements)
 })
 
-test_that("get average hourly concentration from vector value", {
+test_that("Obtener el indice horario de un vector", {
   measurements <- runif(3, min = 1, max = 10)
 
-  expect_equal(calculate_measurements_index(measurements, hours = 1), measurements[1])
+  expect_equal(calculate_measurements_index(measurements, hours = 1), measurements[3])
 })
 
-test_that("get 8-hour moving average concentration from vector value", {
+test_that("Obtiene la concentracion promedio movil de 8 horas", {
   measurements <- runif(8, min = 1, max = 10)
 
   expect_equal(calculate_measurements_index(measurements, hours = 8), mean(measurements))
 })
 
-test_that("gets an and error when sending an hours value less than 1", {
-  measurement <- runif(1, min = 1, max = 10)
+test_that("Obtiene la concentracion promedio movil de 8 horas con 75% de los datos", {
+  measurements <- runif(6, min = 1, max = 10)
 
-  expect_error(calculate_measurements_index(measurement, hours = 0))
+  expect_equal(calculate_measurements_index(measurements, hours = 8), mean(measurements))
 })
 
-test_that("calculate pm10 index", {
-  measurement <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, 10, 16, 13)
+test_that("Obteniendo la concentracion promedio movil de 8 horas con menos 75% de los datos devuelve NA", {
+  measurements <- runif(5, min = 1, max = 10)
 
-  index <- calculate_measurements_index(measurement, hours = 12, weighted = TRUE)
-  expect_equal(round(index), 17)
-
+  expect_equal(calculate_measurements_index(measurements, hours = 8), NA)
 })
 
-test_that("calculate pm10 index 2", {
-  measurement <- c(118, 97, 130, 142, 146, 144, 141, 139, 147, 150, 141, 103)
+test_that("Arroja un error cuando se establece el valor de hours menor a 0", {
+  measurements <- runif(1, min = 1, max = 10)
 
-  index <- calculate_measurements_index(measurement, hours = 12, weighted = TRUE)
-  expect_equal(round(index), 129)
-
+  expect_error(calculate_measurements_index(measurements, hours = 0))
 })
 
-test_that("calculate pm10 index with a missing value", {
-  measurement <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, 10, NA, 13)
-
-  index <- calculate_measurements_index(measurement, hours = 12, weighted = TRUE)
-  expect_equal(round(index), 18)
-
-})
-
-test_that("gets the weight of the measurement", {
+test_that("Calcular promedio movil ponderado 12 horas", {
   measurements <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, 10, 16, 13)
 
-  weight <- obtener_pesos(measurements)
-  expect_equal(weight, 0.5)
+  index <- calculate_measurements_index(measurements, hours = 12, weighted = TRUE)
+  expect_equal(round(index), 17)
 })
 
-test_that("gets the weight of the measurement with an NA value", {
-  measurements <- c(50, 80, 75, 90, 82, NA, 64, 74, 21, 10, 16, 13)
-
-  weight <- obtener_pesos(measurements)
-  expect_equal(weight, 0.5)
-})
-
-test_that("gets the weight of the measurement 2", {
+test_that("Calcular promedio movil ponderado 12 horas (2)", {
   measurements <- c(118, 97, 130, 142, 146, 144, 141, 139, 147, 150, 141, 103)
 
-  weight <- obtener_pesos(measurements)
+  index <- calculate_measurements_index(measurements, hours = 12, weighted = TRUE)
+  expect_equal(round(index), 129)
+})
+
+test_that("Calcular promedio movil ponderado 12 horas con 2 de las 3 horas mas recientes", {
+  measurements <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, 10, NA, 13)
+  expect_false(is.na(calculate_measurements_index(measurements, hours = 12, weighted = TRUE)))
+
+
+  measurements <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, NA, 16, 13)
+  expect_false(is.na(calculate_measurements_index(measurements, hours = 12, weighted = TRUE)))
+
+  measurements <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, 10, 16, NA)
+  expect_false(is.na(calculate_measurements_index(measurements, hours = 12, weighted = TRUE)))
+})
+
+test_that("Calcular promedio movil ponderado 12 horas con menos de 2 de las 3 horas mas recientes devuelve NA", {
+  measurements <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, NA, NA, 13)
+  expect_true(is.na(calculate_measurements_index(measurements, hours = 12, weighted = TRUE)))
+
+  measurements <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, 10, NA, NA)
+  expect_true(is.na(calculate_measurements_index(measurements, hours = 12, weighted = TRUE)))
+
+  measurements <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, NA, 16, NA)
+  expect_true(is.na(calculate_measurements_index(measurements, hours = 12, weighted = TRUE)))
+
+  measurements <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, NA, NA, NA)
+  expect_true(is.na(calculate_measurements_index(measurements, hours = 12, weighted = TRUE)))
+})
+
+test_that("Error si hours es distinto a el tamaño de measurements para el promedio movil ponderado", {
+  measurements <- runif(4, min = 1, max = 10)
+
+  expect_error(calculate_measurements_index(measurements, hours = 12, weighted = TRUE))
+})
+
+test_that("Error si relevant_gap no es un valor aceptado para el promedio movil ponderado", {
+  measurements <- runif(12, min = 1, max = 10)
+  expect_error(
+    calculate_measurements_index(measurements, hours = 12, weighted = TRUE, relevant_gap = 0, min_relevant_records = 0)
+  )
+  expect_error(
+    calculate_measurements_index(measurements, hours = 12, weighted = TRUE, relevant_gap = 14)
+  )
+})
+
+test_that("Error si min_relevant_gap no es un valor aceptado para el promedio movil ponderado", {
+  measurements <- runif(12, min = 1, max = 10)
+  expect_error(
+    calculate_measurements_index(measurements, hours = 12, weighted = TRUE, relevant_gap = 3, min_relevant_records = 0)
+  )
+  expect_error(
+    calculate_measurements_index(measurements, hours = 12, weighted = TRUE, relevant_gap = 3, min_relevant_records = 4)
+  )
+})
+
+test_that("Obtener el factor de ponderacion de 0.5", {
+  measurements <- c(50, 80, 75, 90, 82, 53, 64, 74, 21, 10, 16, 13)
+
+  weight <- get_moving_concentration_weight(measurements)
+  expect_equal(weight, 0.5)
+})
+
+test_that("Obtener el factor de ponderacion de 0.5 con un valor faltante", {
+  measurements <- c(50, 80, 75, 90, 82, NA, 64, 74, 21, 10, 16, 13)
+
+  weight <- get_moving_concentration_weight(measurements)
+  expect_equal(weight, 0.5)
+})
+
+test_that("Obtener el factor de ponderacion mayor a 0.5", {
+  measurements <- c(118, 97, 130, 142, 146, 144, 141, 139, 147, 150, 141, 103)
+
+  weight <- get_moving_concentration_weight(measurements)
   expect_equal(weight, 0.65)
 })
